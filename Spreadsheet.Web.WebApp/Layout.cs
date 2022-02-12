75ba74238472b614577c1ae3ba0d428181481629
Roar <roar.h.eriksen@gmail.com>
2022-02-12 12:16:36 +0100

    Updated UI

--- Spreadsheet.Web.WebApp/Layout.cs
+++ Spreadsheet.Web.WebApp/Layout.cs
@@ -2,7 +2,7 @@ namespace Spreadsheet.Web.WebApp;
 
 public static class Layout
 {
-    public static readonly string FormRow = "row form-row mt-1";
+    public static readonly string FormRow = "row form-row mt-2";
     public static readonly string LabelContainer = "col-2";
     public static readonly string Label = "col-form-label";
         
